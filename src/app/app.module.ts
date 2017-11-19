2a7812e78a9e2814738c6f58fb47085e0669f163
Giizbrellien Meteora Hybrid <sukhum.m@entronica.co.th>
2017-11-19 09:40:11 +0700

    delete login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, OnInit, OnChanges, OnDestroy } from '@angular/core';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
